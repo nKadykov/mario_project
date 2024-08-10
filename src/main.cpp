5725eb58e0ee6d778660c04e3070bce7e7c9664c
nKadykov <nkadykov@gmail.com>
2024-08-10 20:08:45 +0300

    add: window

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Mario");
+    window.setFramerateLimit(60);
+    window.clear();
+    while(window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+            window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+    return 0;
+}
