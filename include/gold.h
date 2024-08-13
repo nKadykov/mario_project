57520bac59efb1bb7540b04a1fd72ef0bba16263
nKadykov <nkadykov@gmail.com>
2024-08-13 21:16:35 +0300

    add: gold

---
+++ include/gold.h
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+
+class Gold
+{
+public:
+    Gold() = delete;
+    Gold(float x, float y) = delete;
+    Gold(const Gold&) = delete;
+    Gold(Gold&&) = default;
+    Gold& operator=(const Gold&) = delete;
+    Gold& operator=(Gold&&) = default;
+    ~Gold() = default;
+
+    void setSprite(sf::Sprite&);
+    void setPosition(int x, int y);
+
+    sf::Sprite getSprite();
+    sf::Vector2f getPosition();
+    sf::FloatRect getBounds();
+
+    void draw(sf::RenderWindow&);
+private:
+    sf::Vector2f position_;
+    sf::Sprite sprite_;
+};
